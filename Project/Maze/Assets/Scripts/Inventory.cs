5b10a9c446211ebb5db773a9032db46c137ace3d
Ahmed Khalaf <ahmedkhalaf@Administrators-Mac-mini.local>
2016-11-06 20:09:19 -0700

    Creating inventory and integrated with the game world

---
+++ Project/Maze/Assets/Scripts/Inventory.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Inventory : MonoBehaviour 
+{
+	ItemDatabase database;
+	GameObject inventoryPanel;
+	GameObject slotPanel;
+	public GameObject inventorySlot;
+	public GameObject inventoryItem;
+
+	public List<Item> items = new List<Item>();
+	public List<GameObject> slots = new List<GameObject>();
+	int slotAmount;
+
+void Start()
+{
+		database = GetComponent<ItemDatabase> ();
+		slotAmount = 3;
+		inventoryPanel = GameObject.Find ("Inventory Panel");
+		slotPanel = inventoryPanel.transform.FindChild ("Slot Panel").gameObject;
+		for (int i = 0; i < slotAmount; i++) 
+		{
+			items.Add (new Item ());
+			slots.Add (Instantiate(inventorySlot));
+			slots[i].transform.SetParent(slotPanel.transform);
+		}
+		AddItem (1);
+		AddItem (0);
+}
+
+	public void AddItem(int id)
+	{
+		Item itemToAdd = database.FetchItemByID (id);
+		for (int i = 0; i < items.Count; i++) 
+		{
+			if (items [i].ID == -1) 
+			{
+				items[i] = itemToAdd;
+				GameObject itemObj = Instantiate(inventoryItem);
+				itemObj.transform.SetParent (slots [i].transform);
+				itemObj.transform.position = Vector2.zero;
+				break;
+			}
+		}
+	}
+}
---
+++ Project/Maze/Assets/Scripts/ItemDatabase.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using LitJson;
+using System.Collections.Generic;
+using System.IO;
+public class ItemDatabase : MonoBehaviour {
+    private List<Item> database = new List<Item>();
+    private JsonData itemData;
+	// Use this for initialization
+	void Start () {
+      //  Item item = new Item(2,"info 3");
+      //  database.Add(item);
+      //  Debug.Log 
+        itemData = JsonMapper.ToObject(File.ReadAllText(Application.dataPath + "/StreamingAssests/item.json"));
+		ConstructItemDatabase();
+		Debug.Log(FetchItemByID(0).ID);
+	}
+
+	public Item FetchItemByID(int id)
+	{
+		for (int i = 0; i < database.Count; i++) 
+
+			if(database[i].ID==id)
+				return database[i];
+		return null;
+	}
+
+    void ConstructItemDatabase()
+    {
+        for (int i= 0; i<itemData.Count;i++)
+        {
+			database.Add(new Item((int)itemData[i]["id"], itemData[i]["title"].ToString(), itemData[i]["description"].ToString()));
+        }  
+    }
+}
+
+
+public class Item
+{
+    public int ID { get; set; }
+    public string Title { get; set; }
+	public string Desc { get; set; }
+
+	public Item(int id, string title, string desc)
+    {
+        this.ID = id;
+        this.Title = title;
+		this.Desc = desc;
+    }
+
+	public Item()
+	{
+		this.ID = -1;
+	}
+}

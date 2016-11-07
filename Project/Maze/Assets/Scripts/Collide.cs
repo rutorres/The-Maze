bb733906caee240480c9672dda781f42ab463ea4
Sultan Alharthi <salharth@nmsu.edu>
2016-11-06 19:34:07 -0700

    Camera fix and player update

---
+++ Project/Maze/Assets/Scripts/Collide.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collide : MonoBehaviour {
+
+	// Use this for initialization
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.name == "t") {
+			Destroy(col.gameObject);
+		}
+	}
+}
---
+++ Project/Maze/Assets/Scripts/Rotateplayer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotateplayer : MonoBehaviour {
+
+	public KeyCode pressUp;
+	public KeyCode pressDown;
+	public KeyCode pressLeft;
+	public KeyCode pressRight;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (pressUp))
+			GetComponent<Transform> ().eulerAngles = new Vector3 (0, 0, 0);
+
+		if (Input.GetKeyDown (pressDown))
+			GetComponent<Transform> ().eulerAngles = new Vector3 (0, 0, 180);
+
+		if (Input.GetKeyDown (pressLeft))
+			GetComponent<Transform> ().eulerAngles = new Vector3 (0, 0, 90);
+
+		if (Input.GetKeyDown (pressRight))
+			GetComponent<Transform> ().eulerAngles = new Vector3 (0, 0, -90);
+	}
+}

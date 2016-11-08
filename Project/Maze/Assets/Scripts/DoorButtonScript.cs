9c7e676ee74379e9a3dfd19135b05bc2a9580af7
Brendan McSherry <brendark89@gmail.com>
2016-11-08 14:40:40 -0700

    Add files via upload

---
+++ Project/Maze/Assets/Scripts/DoorButtonScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorButtonScript : MonoBehaviour {
+
+	// Use this for initialization
+	void OnTriggerEnter2D(Collider2D otherCollider)
+	{
+		Movement2D player = otherCollider.gameObject.GetComponent<Movement2D>();
+		if (player != null) {
+			Destroy (transform.parent.gameObject);
+		}
+
+	}
+
+	void Start()
+	{
+		// 2 - Limited time to live to avoid any leak
+		// Destroy(gameObject, 20); // 20sec
+	}
+}
---
+++ Project/Maze/Assets/Scripts/KeyDoorScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyDoorScript : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D otherCollider)
+	{
+		Movement2D player = otherCollider.gameObject.GetComponent<Movement2D>();
+		if (player != null) {
+			print(KeyCounterScript.score);
+			if (KeyCounterScript.score >= 2) {
+				KeyCounterScript.score -= 2;
+				Destroy (gameObject);
+			}
+		}
+
+	}
+}

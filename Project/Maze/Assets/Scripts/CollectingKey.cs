d1175c59e156d0eefabfb6de19eac4ae18ef138a
IAHMED\Ahmed <al_khalaf30@hotmail.com>
2016-11-09 16:18:47 -0700

    Collecting a Key

---
+++ Project/Maze/Assets/Scripts/CollectingKey.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CollectingKey : MonoBehaviour
+{
+    ItemDatabase database;
+    GameObject inventoryPanel;
+    GameObject keyPanel;
+    public GameObject inventorySlot;
+    public GameObject inventoryItem;
+
+    public List<Item> items = new List<Item>();
+    public List<GameObject> slots = new List<GameObject>();
+    public static int numberofKey = 0;
+
+    void Start()
+    {
+        database = GetComponent<ItemDatabase>();
+        inventoryPanel = GameObject.Find("Inventory Panel");
+        keyPanel = inventoryPanel.transform.FindChild("Key Panel").gameObject;
+        
+            items.Add(new Item());
+            slots.Add(Instantiate(inventorySlot));
+            slots[0].GetComponent<Slot>().id = 0;
+            slots[0].transform.SetParent(keyPanel.transform);
+    }
+
+    public void AddKey(int id)
+    {
+        numberofKey += 1;
+        Item itemToAdd = database.FetchItemByID(id);
+            if (items[0].ID == -1)
+            {
+                items[0] = itemToAdd;
+                GameObject itemObj = Instantiate(inventoryItem);
+                itemObj.GetComponent<ItemData>().item = itemToAdd;
+                itemObj.GetComponent<ItemData>().slot = 0;
+                itemObj.transform.SetParent(slots[0].transform);
+                itemObj.transform.position = slots[0].transform.position;// Vector2.zero;
+                itemObj.GetComponent<Image>().sprite = itemToAdd.Sprite;
+                itemObj.name = itemToAdd.Title;
+            }
+    }
+}
---
+++ Project/Maze/Assets/Scripts/KeyData.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+
+public class KeyData : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerDownHandler
+{
+    public Item item;
+    public int slot;
+    private CollectingKey cKey;
+    private Vector2 _offset;
+
+    void Start()
+    {
+        cKey = GameObject.Find("Collecting Key").GetComponent<CollectingKey>();
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (item != null)
+        {
+            _offset = eventData.position - new Vector2(this.transform.position.x, this.transform.position.y);
+            this.transform.SetParent(this.transform.parent.parent);
+            this.transform.position = eventData.position - _offset;
+            GetComponent<CanvasGroup>().blocksRaycasts = false;
+
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (item != null)
+        {
+            this.transform.position = eventData.position - _offset;
+
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        this.transform.SetParent(cKey.slots[slot].transform);
+        this.transform.position = cKey.slots[slot].transform.position;
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (item != null)
+        {
+            _offset = eventData.position - new Vector2(this.transform.position.x, this.transform.position.y);
+        }
+    }
+}
+
